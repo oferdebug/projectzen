219fe15c43f478faac2f1821e94cc3da1da4e6a7
oferdebug <ofercohen404@gmail.com>
2025-01-29 12:31:33 +0200

    Add forgot password component

---
+++ components/forgot-password/ForgotPasswordForm.tsx
@@ -0,0 +1,91 @@
+'use client';
+import { useState } from 'react';
+import {useRouter} from 'next/navigation';
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import Link from 'next/link';
+import { auth } from '@/lib/auth';
+import { useToast } from '@/hooks/use-toast';
+import { getAuthError } from '@/utils/auth-errors';
+import Icons from '@/components/Icons';
+
+
+export function ForgotPasswordForm() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState('');
+    const router = useRouter();
+    const { toast } = useToast();
+
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            setIsLoading(true);
+            const response = await auth.resetPasswordRequest(email);
+            toast({
+                title: 'Success, check your email!',
+                description: response.message,
+            });
+            router.push('/login');
+        } catch (error) {
+            const { message } = getAuthError(error);
+            toast({
+                variant: 'destructive',
+                title: 'Error',
+                description: message,
+            });
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return (
+        <Card className='w-96'>
+            <form onSubmit={handleSubmit}>
+                <CardHeader className='space-y-1'>
+                    <CardTitle className='text-2xl'>Reset Password</CardTitle>
+                    <CardDescription className='text-sm'>
+                        Enter your email to reset your password, We will send you a link to reset your password.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className='grid gap-4'>
+                    <div className='grid gap-2'>
+                        <Label htmlFor='email'>Email</Label>
+                        <Input
+                            id='Email'
+                            type='Email'
+                            placeholder='Enter Your Email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            disabled={isLoading}
+                            required
+                        />
+                    </div>
+                    <Button className='w-full' type='submit' disabled={isLoading}>
+                        {isLoading && (
+                            <Icons.spinner className='mr-2 h-4 w-4 animate-spin' />
+                        )}
+                        Send Me A Reset Link
+                    </Button>
+                </CardContent>
+                <CardFooter>
+                    <div className='text-sm'>
+                        Remember Your Password{''}
+                        <Link href='/login' className='text-blue-500'>
+                            Sign In
+                        </Link>
+                    </div>
+                </CardFooter>
+            </form>
+        </Card>
+    );
+};
---
+++ components/forgot-password/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ForgotPasswordForm } from "@/components/forgot-password/ForgotPasswordForm";
+
+const ForgotPasswordPage = () => {
+    return (
+        <div className='flex items-center justify-center h-minus-135'>
+            <ForgotPasswordForm />
+        </div>
+    );
+};
+
+export default ForgotPasswordPage;
