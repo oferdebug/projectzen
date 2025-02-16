4b1f1143c8e825d3a5fa5f081c47509f1422298f
oferdebug <ofercohen404@gmail.com>
2025-02-16 20:51:24 +0200

    Before code improvements

--- .next/static/chunks/app_layout_tsx_61af54._.js
+++ .next/static/chunks/app_layout_tsx_61af54._.js
@@ -5,10 +5,10 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/app_layout_tsx_61af54._.js",
   "chunks": [
-    "static/chunks/node_modules_ec66c0._.js",
+    "static/chunks/node_modules_7380d3._.js",
     "static/chunks/[root of the server]__595088._.js",
     "static/chunks/[root of the server]__ca86fc._.css",
-    "static/chunks/node_modules_@tanstack_query-devtools_build_dbb4d7._.js"
+    "static/chunks/node_modules_@tanstack_query-devtools_build_73402e._.js"
   ],
   "source": "dynamic"
 });
